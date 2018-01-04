3c6c222577111839cc46849b7c48dccaf7e51eb3
York Wang <yw544@nyu.edu>
2018-01-04 22:23:59 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
+def hello_t(array)
+if block_given?
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i +1
+  end
+  array
 
+else
+  puts "Hey! No bloc was given!"
+end
 end
 
 # call your method here!
-
